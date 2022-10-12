ca20690b638704ee5acf43e400b875c759446d8a
Gatea Larisa <gatealarisa@gmail.com>
2022-10-12 23:17:24 +0300

    functionality2

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 //Selecting elements
 
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('#score--1');
+const score1El = document.getElementById('score--1');
+const current0El = documnet.getElementById('current--0');
+const current1El = documnet.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -14,14 +16,23 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-//Rolling dice funnctionality
-btnRoll.addEventListener('click', function{
-    //1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
- 
-    //2. Display dice 
-    diceEl.classList.remove('hidden'); 
+let currentScore = 0;
 
-    //3. Check for rolled 1: if true, switch to next player
-    
+//Rolling dice funnctionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `assets/dice-${dice}.png`;
+
+  //3. Check for rolled 1
+  if (dice !== 1) {
+    //Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    //Switch to next player
+  }
 });
